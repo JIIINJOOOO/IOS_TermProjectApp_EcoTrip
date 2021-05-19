b382d01b55eddd57bbc19085519da6ab5c3960e1
Jinjoo Kang <doctory10@naver.com>
2021-05-20 03:39:22 +0900

    스마트폰프로그래밍 텀프로젝트: 메인 화면 구성, 근처 충전소 찾기 탭바 화면 전환 -> 네비게이션 바 메인화면으로 갈때 숨기는 법 구상중 (1.스토리보드에서 top bar = inferred 2.근처충전소뷰에서 네비게이션 아이템으로 눌렀을때 메인 네비게이션 바=hidden 주면 될듯)

---
+++ TermProject/NearStationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NearStationViewController.swift
+//  TermProject
+//
+//  Created by KPUGAME on 2021/05/20.
+//
+
+import UIKit
+
+class NearStationViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+        self.navigationController?.
+    }
+  
+
+}
