1688d4c6c01badd84ead8d6823b47bcc11c87678
Jinjoo Kang <doctory10@naver.com>
2021-05-20 02:09:06 +0900

    Initial Commit

---
+++ TermProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TermProject
+//
+//  Created by KPUGAME on 2021/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+    }
+
+
+}
+
